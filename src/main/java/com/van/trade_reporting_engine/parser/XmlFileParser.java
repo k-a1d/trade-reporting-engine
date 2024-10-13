6c617d664e07668082faa8e60fffa0bf71cb411d
Aidyn Ky <aidynky99@gmail.com>
2024-10-14 01:01:04 +1100

    chore: Remove unnecessary event handler from XML parser

--- src/main/java/com/van/trade_reporting_engine/parser/XmlFileParser.java
+++ src/main/java/com/van/trade_reporting_engine/parser/XmlFileParser.java
@@ -2,8 +2,6 @@ package com.van.trade_reporting_engine.parser;
 
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.Unmarshaller;
-import jakarta.xml.bind.ValidationEvent;
-import jakarta.xml.bind.ValidationEventHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 
@@ -18,12 +16,6 @@ public class XmlFileParser<T> implements FileParser<T> {
     public T parse(File file) {
         JAXBContext jc = JAXBContext.newInstance(resultClass);
         Unmarshaller unmarshaller = jc.createUnmarshaller();
-
-        unmarshaller.setEventHandler(
-            event -> {
-                throw new RuntimeException(event.getMessage(),
-                    event.getLinkedException());
-            });
 
         return resultClass.cast(unmarshaller.unmarshal(file));
     }
