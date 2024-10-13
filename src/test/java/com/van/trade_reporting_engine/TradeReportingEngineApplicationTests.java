e612a795de76c1485f001cbc1b8c44ee35c0e152
Aidyn Ky <aidynky99@gmail.com>
2024-10-13 13:51:56 +1100

    initial commit

---
+++ src/test/java/com/van/trade_reporting_engine/TradeReportingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.van.trade_reporting_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeReportingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
